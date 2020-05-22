d27c449117c2e510c98fbbcee93dd37a0b50f2f3
Billonyohan <yohan.billon@yahoo.fr>
2020-05-22 16:30:35 +0200

    ajout test fonctionnel

---
+++ P11_01_codesource/off/functional_test.py
@@ -0,0 +1,24 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+
+selenium = webdriver.Chrome("/Users/macbookair/Documents/chromedriver")
+selenium.get('https://purbeurreapp.herokuapp.com')
+#find the form element
+username = selenium.find_element_by_id('id_user')
+mail = selenium.find_element_by_id('id_email')
+message = selenium.find_element_by_id('id_message')
+submit = selenium.find_element_by_name('send')
+
+#Fill the form with data
+username.send_keys('Aloe45')
+mail.send_keys('purbeurre.paris@gmail.com')
+message.send_keys('Ca marche !')
+
+#submitting the form
+submit.send_keys(Keys.RETURN)
+#check the returned result
+assert 'Check your email' in selenium.page_source
+selenium.quit()
