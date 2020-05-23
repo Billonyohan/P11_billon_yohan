406cc3ef65d7617658a8c541be6c7de5ac8a8a9e
Billonyohan <yohan.billon@yahoo.fr>
2020-05-23 17:07:10 +0200

    modification functional_test.py plus modification base/login.py

--- P11_01_codesource/off/functional_test.py
+++ P11_01_codesource/off/functional_test.py
@@ -1,22 +1,29 @@
-from django.test import LiveServerTestCase
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time 
 
 
+class FormContactTest(unittest.TestCase):
 
-selenium = webdriver.Chrome("/Users/macbookair/Documents/chromedriver")
-selenium.get('https://purbeurreapp.herokuapp.com')
-#find the form element
-username = selenium.find_element_by_id('id_user')
-mail = selenium.find_element_by_id('id_email')
-message = selenium.find_element_by_id('id_message')
-submit = selenium.find_element_by_name('send')
+    def setUp(self):
+        self.selenium = webdriver.Chrome("/Users/macbookair/Documents/chromedriver")
 
-#Fill the form with data
-username.send_keys('Aloe45')
-mail.send_keys('purbeurre.paris@gmail.com')
-message.send_keys('Ca marche !')
+    def test_form_contact_send(self):
+        selenium = self.selenium
+        selenium.get('https://purbeurreapp.herokuapp.com')
+        username = selenium.find_element_by_id('id_user')
+        mail = selenium.find_element_by_id('id_email')
+        message = selenium.find_element_by_id('id_message')
+        submit = selenium.find_element_by_name('send')
+        username.send_keys('Aloe45')
+        mail.send_keys('purbeurre.paris@gmail.com')
+        message.send_keys('Ca marche !')
+        submit.send_keys(Keys.RETURN)
 
-#submitting the form
-submit.send_keys(Keys.RETURN)
-selenium.quit()
+
+    def tearDown(self):
+        self.selenium.close()
+
+if __name__ == "__main__":
+    unittest.main()
